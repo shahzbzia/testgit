d74fb2e14681e23e872ed008271bc19c53710306
shahzbzia <shahzebzia2000@hotmail.com>
2019-05-21 15:08:58 +0200

    database fix3

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'portfoliozia' );
+define( 'DB_NAME', 'ID211210_portfoliozia' );
 
 /** MySQL database username */
 define( 'DB_USER', 'ID211210_portfoliozia' );
@@ -29,7 +29,7 @@ define( 'DB_USER', 'ID211210_portfoliozia' );
 define( 'DB_PASSWORD', 'Shuzbeta123' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'ID211210_portfoliozia.db.webhosting.be' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
